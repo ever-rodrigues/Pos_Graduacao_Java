5e45aa8016b8c0d4b49a6d0d911a085a76cd9653
evertonrodriguesbrz <e.rodrigues1@pucpr.edu.br>
2022-03-09 10:38:50 -0300

    Initial commit of Pos Graduation Java

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //++x Pré-incremento Incrementa X em um, e então retorna X.
+        //x++ Pós-incremento Retorna X, e então incrementa X em um.
+        int a=4;
+        int b=a++;
+        //x++ - primeiro faz o uso da variavel
+        int c=++a;
+        //++a - primeiro faz a operacao
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
